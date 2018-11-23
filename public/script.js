c11148d5b84554af94b692277c085cefe910d993
Leo Champion <leo.champion11@gmail.com>
2018-11-23 03:02:55 +0100

    dynamic hidden

--- public/script.js
+++ public/script.js
@@ -19,3 +19,72 @@ document.getElementById('button-co').onclick = function () {
     darkColor = clearColor;
     clearColor = tmp;
 };
+for (a of document.querySelectorAll('input[name="4"]')) {
+    a.onclick = function() {
+        h = document.querySelector('input[name="4"]:checked').id !== "4:0";
+        console.log(h)
+        for (let i = 5; i < 7; i++) {
+            console.log(i)
+            document.getElementById('question-' + i).hidden = h;
+            console.log('-----')
+        }
+        for (let i = 11; i < 12; i++) {
+            console.log(i)
+            document.getElementById('question-' + i).hidden = h;
+            console.log('-----')
+        }
+        
+    }
+}
+
+for (a of document.querySelectorAll('input[name="12"]')) {
+    a.onclick = function() {
+        h = document.querySelector('input[name="12"]:checked').id !== "12:1";
+        console.log(h)
+        for (let i = 13; i < 50; i++) {
+            console.log(i)
+            document.getElementById('question-' + i).hidden = h;
+            console.log('-----')
+        }
+        
+    }
+}
+
+for (a of document.querySelectorAll('input[name="13"]')) {
+    a.onclick = function() {
+        h = document.querySelector('input[name="13"]:checked').id !== "13:1";
+        console.log(h)
+        for (let i = 14; i < 31; i++) {
+            console.log(i)
+            document.getElementById('question-' + i).hidden = h;
+            console.log('-----')
+        }
+        
+    }
+}
+
+for (a of document.querySelectorAll('input[name="39"]')) {
+    a.onclick = function() {
+        h = document.querySelector('input[name="39"]:checked').id !== "39:2";
+        console.log(h)
+        for (let i = 40; i < 42; i++) {
+            console.log(i)
+            document.getElementById('question-' + i).hidden = h;
+            console.log('-----')
+        }
+        
+    }
+}
+
+for (a of document.querySelectorAll('input[name="62"]')) {
+    a.onclick = function() {
+        h = document.querySelector('input[name="62"]:checked').id !== "62:1";
+        console.log(h)
+        for (let i = 63; i < 64; i++) {
+            console.log(i)
+            document.getElementById('question-' + i).hidden = h;
+            console.log('-----')
+        }
+        
+    }
+}
